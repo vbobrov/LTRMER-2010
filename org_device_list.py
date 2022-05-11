965f72e4701964b620dd10f05ceb2b3e2f6981fd
Viktor Bobrov <vbobrov@gmail.com>
2022-05-10 21:41:29 -0400

    device list

---
+++ org_device_list.py
@@ -0,0 +1,27 @@
+import texttable
+import meraki
+
+dashboard = meraki.DashboardAPI(suppress_logging=False)
+
+org_id="1045047"
+
+networks=dashboard.organizations.getOrganizationNetworks(org_id)
+network_lookup={network["id"]:network["name"] for network in networks}
+
+devices=dashboard.organizations.getOrganizationDevices(org_id)
+
+device_table=texttable.Texttable(max_width=0)
+#org_table.set_cols_dtype(["i","t"])
+device_table.header(["network","name","model","serial","ip"])
+
+for device in devices:
+    try:
+        ip=device["lanIp"]
+    except KeyError:
+        try:
+            ip=device["wan1Ip"]
+        except KeyError:
+            ip="N/A"
+    device_table.add_row([network_lookup[device["networkId"]],device["name"],device["model"],device["serial"],ip])
+
+print(device_table.draw())
--- org_list.py
+++ org_list.py
@@ -7,7 +7,7 @@ org_list=dashboard.organizations.getOrganizations()
 
 org_table=texttable.Texttable()
 org_table.set_cols_dtype(["i","t"])
-org_table.add_row(["id","name"])
+org_table.header(["id","name"])
 
 for org in org_list:
     org_table.add_row([org['id'],org['name']])
