46f102adaaa80e8d2da0d43f374db5cb4db3dffe
Viktor Bobrov <vbobrov@gmail.com>
2022-05-12 20:37:45 -0400

    ssid updated

---
+++ ssid_update.py
@@ -0,0 +1,42 @@
+import texttable
+import meraki
+import argparse
+
+parser=argparse.ArgumentParser()
+parser.add_argument("-n",metavar="<networkId>",help="Meraki Network ID",required=True)
+parser.add_argument("-s",metavar="<ssid>",help="SSID Name",required=True)
+parser.add_argument("-i",metavar="<ssidnum>",help="SSID Number",type=int,required=True)
+parser.add_argument("-r",metavar="<radiusip>",help="RADIUS Server IP",required=True)
+parser.add_argument("-k",metavar="<radiuskey>",help="RADIUS Server Secret",required=True)
+parser.add_argument("-d",help="Disable SSID",action="store_true",default=False)
+
+args=parser.parse_args()
+
+dashboard = meraki.DashboardAPI(suppress_logging=True)
+
+dashboard.wireless.updateNetworkWirelessSsid(
+    args.n,
+    args.i,
+    name=args.s,
+    enabled=not args.d,
+    authMode="8021x-radius",
+    encryptionMode="wpa",
+    wpaEncryptionMode="WPA1 and WPA2",
+    splashPage="Cisco ISE",
+    radiusServers=[
+        {
+            "host": args.r,
+            "port": 1812,
+            "secret": args.k
+        }
+    ],
+    radiusAccountingServers=[
+        {
+            "host": args.r,
+            "port": 1813,
+            "secret": args.k
+        }
+    ],
+    radiusCoaEnabled=True
+)
+print("SSID Updated Successfully")
--- sw_ports.py
+++ sw_ports.py
@@ -11,9 +11,9 @@ dashboard = meraki.DashboardAPI(suppress_logging=True)
 switch_ports=dashboard.switch.getDeviceSwitchPortsStatuses(args.s)
 
 port_table=texttable.Texttable(max_width=0)
-port_table.header(["Port","Status","Speed","Duplex","LLDP"])
+port_table.header(["Port","Status","Speed","Duplex","CDP","LLDP"])
 
 for port in switch_ports:
-    port_table.add_row([port["portId"],port["status"],port["speed"],port["duplex"],port["lldp"]["systemName"] if "lldp" in port else ""])
+    port_table.add_row([port["portId"],port["status"],port["speed"],port["duplex"],port["cdp"]["platform"] if "cdp" in port else "",port["lldp"]["systemName"] if "lldp" in port else ""])
 
 print(port_table.draw())
